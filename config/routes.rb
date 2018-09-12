6ba8db4891765397ba91d6e8ce41fb7e4451e0df
Daniel Moreto <dfmoreto@gmail.com>
2018-09-12 02:39:49 -0300

    Create controller for Like and add its routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,20 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      concern :likeable do |options|
+        shallow do
+          post "/likes", { to: "likes#create", on: :member }.merge(options)
+          delete "/unlikes", { to: "likes#destroy", on: :member }.merge(options)
+        end
+      end
+
       resources :users, only: [] do
         resources :followings, only: %i(create destroy), shallow: true
       end
 
-      resources :posts, only: %i(create show update destroy)
-
+      resources :posts, only: %i(create show update destroy) do
+        concerns :likeable, likeable_type: 'Post'
+      end
     end
   end
 end
