2046c4f4e9cdc066eb7933b019f07e27db67c19b
Daniel Moreto <dfmoreto@gmail.com>
2018-09-12 03:30:53 -0300

    Add an after_create to create Hashtags following Post description

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,21 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, as: :likeable, dependent: :delete_all
+  has_many :hashtag_mappings, as: :hashtag_mapping
 
   has_one_attached :photo
 
   validates :photo, file_presence: true
   validates :description, presence: true
+
+  after_create :map_hashtags!
+
+
+  private
+
+
+  def map_hashtags!
+    service = HashtagMappingService.new(self, :description)
+    service.call
+  end
 end
