93697b1ee4fe0b25c01e72906e8093bf214b8629
Daniel Moreto <dfmoreto@gmail.com>
2018-09-12 02:48:47 -0300

    Add message on :follower uniqueness validator on Follow model

--- app/models/following.rb
+++ app/models/following.rb
@@ -2,6 +2,6 @@ class Following < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
-  validates :followed, uniqueness: { scope: :follower_id }
+  validates :follower_id, uniqueness: { scope: :followed_id, message: "can't refollow a user" }
   validates_with SelfFollowingValidator
 end
