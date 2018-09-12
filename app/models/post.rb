96db7ba426559d2dc537176e39efb732983b2d24
Daniel Moreto <dfmoreto@gmail.com>
2018-09-12 15:54:49 -0300

    Update model Post to map hastags after_save and to add association with :hashtags

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, as: :likeable, dependent: :delete_all
   has_many :hashtag_mappings, as: :hashtagable
+  has_many :hashtags, through: :hashtag_mappings
 
   has_one_attached :photo
 
   validates :photo, file_presence: true
   validates :description, presence: true
 
-  after_create :map_hashtags!
+  after_save :map_hashtags!
 
 
   private
