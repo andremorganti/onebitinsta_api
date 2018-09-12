1d14ca893d384b65453d645ca5ce770e4f3ded64
Daniel Moreto <dfmoreto@gmail.com>
2018-09-12 16:27:15 -0300

    Permit additional params on User registratio on account update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name, :photo, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :name, :photo, :description])
+  end
 end
